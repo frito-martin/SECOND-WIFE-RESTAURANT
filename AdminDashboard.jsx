2adbf95dc9662ffc0f66610a6d0c0139b0c0777e
Muzei frito <139154552+frito-martin@users.noreply.github.com>
2023-11-22 15:06:50 -0500

    Add files via upload

---
+++ AdminDashboard.jsx
@@ -0,0 +1,15 @@
+// AdminDashboard.js
+
+import React from 'react';
+
+const AdminDashboard = () => {
+  // In a real app, fetch and display relevant data for the admin
+  return (
+    <div>
+      <h2>Admin Dashboard</h2>
+      {/* Display admin-related information and functionalities */}
+    </div>
+  );
+};
+
+export default AdminDashboard;
